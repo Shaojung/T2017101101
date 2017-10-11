daeb7970969a3b2da47c65ca56580b575e0b5a0f
Shaojung Lu <shaojung@gmail.com>
2017-10-11 13:24:05 +0800

    Initial commit

---
+++ app/src/main/java/com/example/teacher/t2017101101/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.teacher.t2017101101;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void click1(View v)
+    {
+        new Thread(){
+            @Override
+            public void run()
+            {
+                try {
+                    URL url = new URL("https://www.google.com.tw");
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.connect();
+                    InputStream is = conn.getInputStream();
+                    InputStreamReader isr = new InputStreamReader(is);
+                    BufferedReader br = new BufferedReader(isr);
+
+                    String str = br.readLine();
+                    Log.d("MYNET", str);
+                    br.close();
+                    isr.close();
+                    is.close();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }.start();
+
+
+    }
+}
